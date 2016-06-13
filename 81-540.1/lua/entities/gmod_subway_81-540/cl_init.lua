5652f866d4146a09c3970eff1aed39599176b95a
Bruce Kelt <trainkbruce@gmail.com>
2016-06-13 21:09:06 +1000

    -Added 81-540.1 Model (No Doors) and yet no doors props made. *Included 81-540.1 Tara Script.

---
+++ 81-540.1/lua/entities/gmod_subway_81-540/cl_init.lua
@@ -0,0 +1,24 @@
+﻿include("shared.lua")
+
+
+--------------------------------------------------------------------------------
+ENT.ClientProps = {}
+ENT.ButtonMap = {}
+
+--------------------------------------------------------------------------------
+ENT.ClientPropsInitialized = false
+
+--------------------------------------------------------------------------------
+function ENT:Think()
+	self.BaseClass.Think(self)
+end
+
+function ENT:Draw()
+	self.BaseClass.Draw(self)
+end
+
+function ENT:OnButtonPressed(button)
+	if button == "ShowHelp" then
+		RunConsoleCommand("metrostroi_train_manual")
+	end
+end
