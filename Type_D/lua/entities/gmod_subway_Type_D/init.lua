83ea119036e3ec01f260f363882802d525a3e92b
Bruce Kelt <trainkbruce@gmail.com>
2016-07-11 18:57:03 +1000

    -Added Type D Train.

---
+++ Type_D/lua/entities/gmod_subway_Type_D/init.lua
@@ -0,0 +1,111 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include("shared.lua")
+
+ENT.BogeyDistance = 650 -- Needed for gm trainspawner
+
+--------------------------------------------------------------------------------
+function ENT:Initialize()
+	-- Defined train information
+	self.SubwayTrain = {
+		Type = "Type D",
+		Name = "Type D",
+		WagType = 1,
+	}
+
+	-- Set model and initialize
+	self:SetModel("models/train/russian/type_d.mdl")
+	self.BaseClass.Initialize(self)
+	self:SetPos(self:GetPos() + Vector(0,0,140))
+	
+	-- Create seat entities
+	self.DriverSeat = self:CreateSeat("driver",Vector(445,-40,85)) 
+	self.InstructorsSeat = self:CreateSeat("instructor",Vector(445,40,85))
+
+	-- Hide seats
+	self.DriverSeat:SetColor(Color(255,255,255,255))
+	self.DriverSeat:SetRenderMode(RENDERMODE_TRANSALPHA)
+	self.InstructorsSeat:SetColor(Color(255,255,255,255))
+	self.InstructorsSeat:SetRenderMode(RENDERMODE_TRANSALPHA)
+
+	
+	-- Create Bogeys
+	self.FrontBogey = self:CreateBogey(Vector( 450-120,0,31.5),Angle(0,180,0),true) 
+	self.RearBogey = self:CreateBogey(Vector(-485+155,0,31.5),Angle(0,0,0),false)
+	
+	-- Initialize key mapping
+	self.KeyMap = {
+		[KEY_W] = "Drive", 
+		[KEY_S] = "Brake", 
+		[KEY_R] = "Reverse",
+		[KEY_L] = "HornEngage",
+		}
+	
+end
+
+	
+	function ENT:Think()
+   	local retVal = self.BaseClass.Think(self)
+		
+		
+		-- Generate bogey sounds
+	local jerk = math.abs((self.Acceleration - (self.PrevAcceleration or 0)) / self.DeltaTime)
+	self.PrevAcceleration = self.Acceleration
+
+	if jerk > (2.0 + self.Speed/15.0) then
+		self.PrevTriggerTime1 = self.PrevTriggerTime1 or CurTime()
+		self.PrevTriggerTime2 = self.PrevTriggerTime2 or CurTime()
+
+		if ((math.random() > 0.00) or (jerk > 10)) and (CurTime() - self.PrevTriggerTime1 > 1.5) then
+			self.PrevTriggerTime1 = CurTime()
+			self.FrontBogey:EmitSound("subway_trains/chassis_"..math.random(1,3)..".wav", 70, math.random(90,110))
+		end
+		if ((math.random() > 0.00) or (jerk > 10)) and (CurTime() - self.PrevTriggerTime2 > 1.5) then
+			self.PrevTriggerTime2 = CurTime()
+			self.RearBogey:EmitSound("subway_trains/chassis_"..math.random(1,3)..".wav", 70, math.random(90,110))
+		end
+	end
+
+
+	-- RUT test
+	local weightRatio = 2.00*math.max(0,math.min(1,(self:GetNW2Float("PassengerCount",0)/300)))
+	if math.abs(self:GetAngles().pitch) > 2.5 then weightRatio = weightRatio + 1.00 end
+	self.YAR_13A:TriggerInput("WeightLoadRatio",math.max(0,math.min(2.50,weightRatio)))
+	-- Exchange some parameters between engines, pneumatic system, and real world
+	self.Engines:TriggerInput("Speed",self.Speed)
+	if IsValid(self.FrontBogey) and IsValid(self.RearBogey) then
+		self.FrontBogey.MotorForce = 65300
+		self.FrontBogey.Reversed = (self.RKR.Value > 0.5)
+		self.RearBogey.MotorForce  = 65300
+		self.RearBogey.Reversed = (self.RKR.Value < 0.5)
+		
+		-- These corrections are required to beat source engine friction at very low values of motor power
+		local A = 2*self.Engines.BogeyMoment
+		local P = math.max(0,0.04449 + 1.06879*math.abs(A) - 0.465729*A^2)
+		if math.abs(A) > 0.4 then P = math.abs(A) end
+		if math.abs(A) < 0.05 then P = 0 end
+		if self.Speed < 10 then P = P*(1.0 + 0.5*(10.0-self.Speed)/10.0) end
+		self.RearBogey.MotorPower  = P*0.5*((A > 0) and 1 or -1)
+		self.FrontBogey.MotorPower = P*0.5*((A > 0) and 1 or -1)
+		--self.RearBogey.MotorPower  = P*0.5
+		--self.FrontBogey.MotorPower = P*0.5
+
+		--self.Acc = (self.Acc or 0)*0.95 + self.Acceleration*0.05
+		--print(self.Acc)
+
+		-- Apply brakes
+		self.FrontBogey.PneumaticBrakeForce = 40000.0
+		self.FrontBogey.BrakeCylinderPressure = self.Pneumatic.BrakeCylinderPressure
+		self.FrontBogey.BrakeCylinderPressure_dPdT = -self.Pneumatic.BrakeCylinderPressure_dPdT
+		self.FrontBogey.ParkingBrake = self.ParkingBrake.Value > 0.5
+		self.RearBogey.PneumaticBrakeForce = 40000.0
+		self.RearBogey.BrakeCylinderPressure = self.Pneumatic.BrakeCylinderPressure
+		self.RearBogey.BrakeCylinderPressure_dPdT = -self.Pneumatic.BrakeCylinderPressure_dPdT
+		--self.RearBogey.ParkingBrake = self.ParkingBrake.Value > 0.5
+		end
+	end
+--------------------------------------------------------------------------------
+function ENT:Think()
+	local retVal = self.BaseClass.Think(self)
+	return retVal
+end
---
+++ Type_D/lua/metrostroi/systems/sys_type_d_system.lua
@@ -0,0 +1,33 @@
+--------------------------------------------------------------------------------
+-- Placeholder for Tatra-T3 systems
+--------------------------------------------------------------------------------
+Metrostroi.DefineSystem("Type_D_Systems")
+TRAIN_SYSTEM.DontAccelerateSimulation = true
+
+function TRAIN_SYSTEM:Initialize()
+	self.Drive = 0
+	self.Brake = 0
+	self.Reverse = 0
+end
+
+
+function TRAIN_SYSTEM:Inputs()
+	return { "Drive", "Brake","Reverse" }
+end
+
+function TRAIN_SYSTEM:TriggerInput(name,value)
+	if self[name] then self[name] = value end
+end
+
+
+
+--------------------------------------------------------------------------------
+function TRAIN_SYSTEM:Think()
+	--print("DRIVE",self.Drive)
+	self.Train.FrontBogey.MotorForce = 52125
+	self.Train.FrontBogey.MotorPower = self.Drive - self.Brake
+	self.Train.FrontBogey.Reversed = (self.Reverse > 0.5)
+	self.Train.RearBogey.MotorForce  = 52125
+	self.Train.RearBogey.MotorPower = self.Drive - self.Brake
+	self.Train.RearBogey.Reversed = not (self.Reverse > 0.5)
+end
